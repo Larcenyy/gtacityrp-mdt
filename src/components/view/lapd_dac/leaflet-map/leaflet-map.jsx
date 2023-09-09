e91335fd268eca149e5f43fa6425d8369a14fd8a
Remy <remyc59440@hotmail.com>
2023-09-09 16:50:24 +0200

    Ajout du iframe pour la map dans les différents pages du MDT

--- src/components/view/lapd_dac/leaflet-map/leaflet-map.jsx
+++ src/components/view/lapd_dac/leaflet-map/leaflet-map.jsx
@@ -6,7 +6,7 @@ function MapComponent() {
                 src="/map.html"
                 title="Map"
                 width="100%"
-                height="370px"
+                height="100%"
                 frameBorder="0"
                 scrolling="no"
             ></iframe>
