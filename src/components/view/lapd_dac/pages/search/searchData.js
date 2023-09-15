69a3e1287b74a63634f366e1985ac509765910f1
Remy <remyc59440@hotmail.com>
2023-09-15 19:19:37 +0200

    Ajout de la page recherche avec ses fonctionnalités

---
+++ src/components/view/lapd_dac/pages/search/searchData.js
@@ -0,0 +1,32 @@
+export const searchData = [
+    {
+        title: "Jhohn Doe",
+        date: "01/01/1999",
+        filtre: "Rapport"
+    },
+    {
+        title: "Jhohn Doe",
+        date: "01/01/1999",
+        filtre: "Rapport"
+    },
+    {
+        title: "Jhohn Doe",
+        date: "01/01/1999",
+        filtre: "Rapport"
+    },
+    {
+        title: "Jhohn Doe",
+        date: "01/01/1999",
+        filtre: "Rapport"
+    },
+    {
+        title: "Jhohn Doe",
+        date: "01/01/1999",
+        filtre: "Rapport"
+    },
+    {
+        title: "Jhohn Doe",
+        date: "01/01/1999",
+        filtre: "Rapport"
+    },
+];
